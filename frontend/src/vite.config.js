3f63b73e5e23a4a4aec9a1b678100873d8245f7a
metisssi <simcheraandrij@gmail.com>
2026-02-25 14:40:58 +0100

    reccoent db

--- frontend/src/vite.config.js
+++ frontend/src/vite.config.js
@@ -3,12 +3,17 @@ import { resolve } from 'path';
 
 export default defineConfig({
   appType: 'mpa',
+  server: {
+    proxy: {
+      // Все запросы к /api теперь будут автоматически улетать на PHP
+      '/api': 'http://localhost:8000'
+    }
+  },
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        // Update this line to match your folder structure:
-        admin: resolve(__dirname, 'src/admin/admin.html')
+        admin: resolve(__dirname, 'admin.html')
       }
     }
   }
